2b77f3632f8bc6caa14a12d282482ea770cfe844
Thermiprettie <tadekolasarah@yahoo.com>
2022-12-05 12:01:21 -0800

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks palindrome
+ * @head: code head
+ * Return: 0, or 1, if otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	unsigned int len = 1;
+	listint_t *temp;
+
+	if (head == NULL|| *head == NULL)
+		return (1);
+
+	temp = *head;
+	while (temp)
+	{
+		temp = temp->next;
+		len++;
+	}
+	return (0);
+}
