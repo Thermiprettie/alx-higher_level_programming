49c8f7af3f38599aae312e087ae95e2ef83d6c1c
Sarah Adeola <tadekolasarah@yahoo.com>
2022-12-19 14:09:40 -0800

    added

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+
+
 def magic_calculation(a, b):
-    calc = 0
+    result = 0
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
             else:
-                calc += (a ** b) / i
+                result += (a ** b) / i
         except:
-            calc = b + a
+            result = b + a
             break
-    return calc
+    return result
