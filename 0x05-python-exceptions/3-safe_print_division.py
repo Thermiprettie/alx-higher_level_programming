bfe41318f207aa90cee5a42b4b76e3aa1819e656
Sarah Adekola <tadekolasarah@yahoo.com>
2022-12-19 18:42:19 +0000

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,4 +6,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         intdiv = None
     finally:
-        print("Inside result: {}".format(div))
+        print("Inside result: {}".format(intdiv))
