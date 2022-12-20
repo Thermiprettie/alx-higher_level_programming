81742399dbad0f339d9fb5ab9bc7de137aab693e
Sarah Adeola <tadekolasarah@yahoo.com>
2022-12-20 07:38:57 -0800

    add

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
 class Square:
+    """ This create an attribute
+
+    for the square
+    Attribute:
+        __size (int): size of a square"""
+
     def __init__(self, size):
+        """Initializes the square"""
         self.__size = size
