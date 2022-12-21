ef3ffd650c2cac8b965d60baca7a4f7e4109dd3c
Sarah Adeola <tadekolasarah@yahoo.com>
2022-12-21 06:07:53 -0800

    add task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-class Square:
-    """ This create an attribute
+"""Create class Square"""
 
-    for the square
-    Attribute:
-        __size (int): size of a square"""
+
+class Square:
+    """ This represent a square"""
 
     def __init__(self, size):
-        """Initializes the square"""
+        """Initializes the square
+
+        Args:
+            size (int): size of the square.
+        """
         self.__size = size
