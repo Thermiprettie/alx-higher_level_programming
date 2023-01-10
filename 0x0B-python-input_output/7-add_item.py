a16d244fb858528c4fa03322a264de8952d3d497
Sarah Adekola <tadekolasarah@yahoo.com>
2023-01-10 23:15:17 +0000

    indentation error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Define the function to add arguments to a Python list."""
-import json
+import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
