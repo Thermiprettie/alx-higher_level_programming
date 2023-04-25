22ccb3b5901493dd8619fead844a887170242319
Thermiprettie <tadekolasarah@yahoo.com>
2023-04-25 14:50:33 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,6 +5,6 @@ const fs = require('fs');
 const fileName = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(fileName, content, 'utf8', (err) => {
+fs.writeFile(fileName, content, 'utf8', function(err) {
   if (err) throw err;
 });
