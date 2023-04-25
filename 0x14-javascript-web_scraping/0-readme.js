f7f29a782ff07d4da2cfeb1e709a4c84120433d3
Thermiprettie <tadekolasarah@yahoo.com>
2023-04-25 14:28:17 -0700

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file
-// content of the file must be read in utf-8
 
-var fileName = process.argv[2];
+const fileName = process.argv[2];
 // Include fs module
-var fs = require('fs');
+const fs = require('fs');
   
 // Use fs.readFile() method to read the file
 fs.readFile(fileName, 'utf8', function(err, data) {
