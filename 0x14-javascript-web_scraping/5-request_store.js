12f040eef8f8f41c260716f87f049cb9f3cb34f7
Thermiprettie <tadekolasarah@yahoo.com>
2023-04-25 15:41:23 -0700

    0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage
+// and stores it in a file
+const request = require('request');
+const fs = require('fs');
+const route = process.argv[2];
+const fileName = process.argv[3];
+
+request(route, (err, res, body) => {
+  if (err) throw err;
+
+// Use fs.writeFile() method to write the file
+  fs.writeFile(file, body, 'utf8', (err) => {
+    if (err) throw err;
+  });
+});
